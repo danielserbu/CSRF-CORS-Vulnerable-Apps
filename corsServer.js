11ff0c28f0ff23d552f8453c9e4d55a5fbdb14db
daniserbu <daniserbu@outlook.com>
2024-03-26 16:11:28 +0200

    some changes

--- corsServer.js
+++ corsServer.js
@@ -8,6 +8,7 @@ const port = 3000;
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  //res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
